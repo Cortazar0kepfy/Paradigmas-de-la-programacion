9d395ec8036056f8efd3101727efc0b075695eaa
Cortazar0kepfy <cantorzabellin@gmail.com>
2024-10-25 02:28:48 -0600

    Actualizar 010_asociacion.py

--- 010_asociacion.py
+++ 010_asociacion.py
@@ -1,80 +1,48 @@
-  
-
-
-
-
 
 class A:
-    
-    _a:float=0.0
-    _b:float=0.0
-    _c:float=0.0
-
-
-    def _init_(self ,a:float,b:float,c:float):
-  	self.a = a
-	self.b = b
-	self.c = c
-
-    
-
+    def __init__(self, a: float, b: float, c: float):
+        self.a = a
+        self.b = b
+        self.c = c
 
 class B:
-    _d:float=0.0
-    _e:float=0.0
-    def _init_(self,d:float,e:float)
- 	self.d = d
-	self.e = e
-     def sumar_todo(self,aa:float, bb:float):
-	x:float=self.d+self.e+aa+bb
-	return x
-#Asociacion.............
-objetoA = A(1.0,2.0,3.0)
-objetoB = B(4.0,5.0)
-print(objetoB.sumar_todo(objetoA.a,objeto.b))
+    def __init__(self, d: float, e: float):
+        self.d = d
+        self.e = e
 
+    def sumar_todo(self, aa: float, bb: float) -> float:
+        return self.d + self.e + aa + bb
+
+# Asociación
+objetoA = A(1.0, 2.0, 3.0)
+objetoB = B(4.0, 5.0)
+print(objetoB.sumar_todo(objetoA.a, objetoA.b))
 
 class C:
-    _d:float=0.0
-    _e:float=0.0
-    _Aa:A=None
-    def _init_(self,d:float,e:float):
-	self.d = d
-	self.e = e
-	self.Aa = A(1.0,2.0,3.0)
-    def sumar_todo(self):
-	x:float=self.d+self.e+self.Aa.a+self.Aa.b
-	return x
+    def __init__(self, d: float, e: float):
+        self.d = d
+        self.e = e
+        self.Aa = A(1.0, 2.0, 3.0)
 
+    def sumar_todo(self) -> float:
+        return self.d + self.e + self.Aa.a + self.Aa.b
 
-
-#composicion...............
-
-
-
-objetoC = C(4.0,5.0)
-print(objetoC.sumar_todo()) 
-
+# Composición
+objetoC = C(4.0, 5.0)
+print(objetoC.sumar_todo())
 
 class D:
-    _d:float=0.0
-    _e:float=0.0
-    _Aa:A=None
-    
-    def _init_(self,d:float,e:float,Aa:A):
-	self.d = d
-	self.e = e
-	self.Aa = Aa
-    def sumar_todo(self):
-	x:float=self.d+self.e+self.Aa.a+self.Aa.b
-	return x
+    def __init__(self, d: float, e: float, Aa: A):
+        self.d = d
+        self.e = e
+        self.Aa = Aa
 
+    def sumar_todo(self) -> float:
+        return self.d + self.e + self.Aa.a + self.Aa.b
 
-#Agregacion..........................
-objeto D = D(4.0,5.0,objetoA)
+# Agregación
+objetoD = D(4.0, 5.0, objetoA)
 print(objetoD.sumar_todo())
-
-	x:float=self.d+self.e+self.Aa.a+self
 
 
 

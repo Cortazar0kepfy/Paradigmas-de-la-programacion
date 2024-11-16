53aa7d549749f4dabbea9cf76725f150b50475f4
kipuna <cantorzabellin@gmail.com>
2024-11-16 12:51:20 -0600

    Update 09_Constructor.py

--- 09_Constructor.py
+++ 09_Constructor.py
@@ -1,36 +1,27 @@
-#Cortázar Tinajero Luis Enrique...........
+# Cortázar Tinajero Luis Enrique...........
 #      LMA.
 
-
-#clase computadora.........
+# Clase Computadora
 class Computadora:
     marca: str = None
     capacidad: int = 0
     ram: int = 0
 
-
-
-
-
-
-#Constructor.....................
-
-    def __init__(self, marca:str, capacidad:int, ram:int):
-        print("Accediendo al constructor de la pc:{marca}")
+    # Constructor
+    def __init__(self, marca: str, capacidad: int, ram: int):
+        print(f"Accediendo al constructor de la pc: {marca}")
         self.marca = marca
         self.capacidad = capacidad
         self.ram = ram
 
     def imprimirInfoPC(self):
-        print(f"Está es la Computadora marca: {self.marca} con amacenamiento de {self.capacidad y memoria de {self.ram}GB")
+        print(f"Esta es la computadora marca: {self.marca} con almacenamiento de {self.capacidad}GB y memoria de {self.ram}GB")
 
-#   .Destructor.......................
+    # Destructor
     def __del__(self):
-        print(f"Se eliminó la computadora: {self.marca"}
+        print(f"Se eliminó la computadora: {self.marca}")
 
-
-
-#.....Clase Persona
+# Clase Persona
 class Persona:
     nombres: str = None
     apellidos: str = None
@@ -38,44 +29,33 @@ class Persona:
     direccion: str = None
     computadora: Computadora = None
 
-#Constructor de persona...................
-
-    def __init__(self,nombres:str, apellidos:str, edad:int, direccion:str, marca:str, capacidad:int, ram:int):
+    # Constructor de persona
+    def __init__(self, nombres: str, apellidos: str, edad: int, direccion: str, marca: str, capacidad: int, ram: int):
         self.nombres = nombres
         self.apellidos = apellidos
         self.edad = edad
         self.direccion = direccion
-        self.Computadora = Computadora(marca, capacidad, ram)
-        print(f"---Accedimos al constructor de la persona: {nombre {apellidos}")
+        self.computadora = Computadora(marca, capacidad, ram)
+        print(f"---Accedimos al constructor de la persona: {nombres} {apellidos}")
 
     def imprimirInfo(self) -> None:
-        print(f"--Mi nombre es {self.nombres} {self.apellidos},tengo {self.edad} años de edad, vivo en {self.dirección} ")
-        
-        self.Computadora.imprimirInfoPC()
+        print(f"--Mi nombre es {self.nombres} {self.apellidos}, tengo {self.edad} años de edad, vivo en {self.direccion}")
+        self.computadora.imprimirInfoPC()
 
-
-#Destructor
+    # Destructor
     def __del__(self):
         print(f"---Eliminamos a la persona... {self.nombres} {self.apellidos}")
 
-
-
-#función 1 es el programa...........................
+# Función 1 es el programa
 def funcion1():
-    persona = Persona("Carlos","Perez","40","Xochimilco","Lenovo",700,8)
+    persona = Persona("Carlos", "Perez", 40, "Xochimilco", "Lenovo", 700, 8)
     print("\n")
     persona.imprimirInfo()
     print("\n")
-    persona2= Persona("Magdalena","Carrillo",35,"Jalapa","IBM",200,4)
+    persona2 = Persona("Magdalena", "Carrillo", 35, "Jalapa", "IBM", 200, 4)
     print("\n")
     persona2.imprimirInfo()
     print("\n")
-#:::::::::::::::::::::::::::::::::::::::::::::::::::
 
-#Llamar a función1
-
-#:::::.............................................
-
+# Llamar a función1
 funcion1()
-
-

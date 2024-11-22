2ed37d73f5cfc3dc6e22d475027cfab64b7418a1
cortazar0kepfy <cantorzabellin@gmail.com>
2024-11-21 19:34:47 -0600

    puto el que lo lea

---
+++ 14__interface.py
@@ -0,0 +1,59 @@
+#Cortázar Tinajero Luis Enrique.
+#Interface...........
+
+#Del directorio aplicacion  el subdirectorio repositorio,
+# archivo basededatos.py : trae el onjeto BaseDeDatos.
+from.aplicacion.repositorio.basededatos import BaseDeDatos
+
+
+
+
+from aplicacion.repositorio.s3 import S3
+
+
+
+
+from aplicacion.repositorio.sistemadearchivos import SistemaDeArchivos
+
+
+
+
+from aplicacion.usuario import Usuario
+
+
+from aplicacion.negocios.manejodeinscripciones import ManejoDeInscripciones
+
+
+
+#Creando el objeto usuario
+usuario = Usuario("Roberto", "Jimenez",18)
+
+
+#Creando el objeto s3.............()()()()()()()()()()()()()()(()()()(
+repositorios3 = S3("321321321", "sdf324223", "MiCubeta")
+
+
+
+
+#Proceso de Interface..............................()()()()()()()()()()()()()()()()()()()()()()()()
+
+ManejoDeInscripciones.inscribirUsuario(usuario,repositorioS3)
+print("\n")
+
+#Crea el objeto SistemaDeArchivos...........
+repositorioSistemaDeArchivos = SistemaDeArchivos("/home/users")
+
+
+
+ManejoDeInscripciones.inscribirUsuario(usuario,repositorioSistemaDeArchivos)
+print("\n")
+
+
+#Crea el objeto basedatos.......-----------
+repositorioBaseDeDatos = BaseDeDatos("localhost", "admin", "admin123")
+
+
+
+ManejoDeInscripciones.inscribirUsuario(usuario,repositorioBaseDeDatos)
+
+

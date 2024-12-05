c09161034db9c14f0d4e24d0bc8d793de232d7d2
cortazar0kepfy <cantorzabellin@gmail.com>
2024-12-05 07:08:53 -0600

    sakaaaaaa

---
+++ 18_generador.py
@@ -0,0 +1,26 @@
+#Cortázar Tinajero Luis Enrique..
+
+
+# yield transforma la función a iterador..
+
+
+def migenerador():
+    print("Primero")
+    yield 10
+    print("Segundo")
+    yield "20"
+    print("Tercero")
+    yield "hola"
+
+
+#gen es un iterador......()/())(
+
+
+gen = migenerador()
+val1 = next(gen)
+print(val1)
+val2 = next(gen)
+print(val2)
+val3 = next(gen)
+print(val3)
+

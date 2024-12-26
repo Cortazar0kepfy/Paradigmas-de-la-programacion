95c2f35f3c72052feb55430aefd055dbebf91c76
cortazar0kepfy <cantorzabellin@gmail.com>
2024-12-25 19:08:12 -0600

    suburna en la kirpel

--- aplicacion/modelos/usuario.py
+++ aplicacion/modelos/usuario.py
@@ -5,26 +5,22 @@ class Usuario:
     __edad: int
 
 
-
-
-#Constructor.
-
-
-def __init__(mi, nombre:str, apellido: str, edad: int):
-    mi.__nombre = nombre
-    mi.__apellido = apellido
-    mi.__edad = edad
+    def __init__(mi, nombre: str, apellido: str, edad: int):
+        mi.__nombre = nombre
+        mi.__apellido = apellido
+        mi.__edad = edad
 
 
 
-#getters...........()()()()()()()()()()()()()()()()()
-def getNombre(mi) -> str:
-    return mi.__nombre
-
-def getApellido (mi) -> str:
-    return mi.__apellido
+# ## # ##getters
 
 
-def getEdad (mi) -> str:
-    return mi.__edad
+        def getNombre)mi) ->str:
+            return mi.__nombre
 
+
+        def getApellido(mi) ->str:
+            return mi.__apellido
+
+        def getEdad(mi) ->str:
+            return mi.__edad

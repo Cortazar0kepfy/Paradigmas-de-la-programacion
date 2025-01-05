0e9e76957782044d251dc78153624c19a1702c32
cortazar0kepfy <cantorzabellin@gmail.com>
2025-01-05 15:04:44 -0600

    Telma

--- 28_MontecarloCuda.py
+++ 28_MontecarloCuda.py
@@ -35,4 +35,33 @@ def calcularpi_kernel(rng_states, iteraciones, out):
     """Escribir resultado para proceso ii
     #=============================
     """
-    out[ii] = 4.0 * cae_adentro
+    out[ii] = 4.0 * cae_adentro / iteraciones
+
+#================    
+"""Proceso para cuda >>>>>>>>>>>>>>><<<<<<<>>>>>>>>>>
+"""
+
+N = 262144
+hilosporbloque = 128
+bloques = int(N/hilosporbloque)
+
+
+
+#Semillas....///////////////////////
+
+seed1 = random.uniform(-1,1)
+print(seed1)
+seed2 = random.seed(seed1)
+print(seed2)
+seed = random.randint(-1000,1000)
+print(seed)
+rng_states = create_xoroshiro128p_states(hilosporbloque*bloques, seed)
+
+"""Arreglos de salida (escritura)
+"""
+out = np.zeros(hilosporbloque*bloques, dtype=np.float64)
+
+#Correr en paralelo el kernel de cuda..
+calcularpi_kernel[bloques, hilosporbloque](rng_states, 100000, out)
+print('pi:', out.mean())
+

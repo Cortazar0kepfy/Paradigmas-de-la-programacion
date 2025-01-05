b8d49381e3c226fca09a3278657052d3b3527c26
cortazar0kepfy <cantorzabellin@gmail.com>
2025-01-05 15:49:13 -0600

    En las almas de la diéresis del kipolka

--- 29MPIbasico.py
+++ 29MPIbasico.py
@@ -1,51 +1,35 @@
-#Cortázar Tinajero Luis Enrique.
+#Cortázar Tinajero Luis Enrique-
 
-"""OJo !!! Para correr se escribe:
-    mpiexec -n 4 python3 27_mpibasico.py
-   
-   Pero si quieres más procesos que procesadores
-   mpirun --oversubscribe  -n 400 pyhton3 27_mpibasico.py
 
-   Para correr en cuatro procesos.
-"""
 
+
+
+
+
+# Importar la biblioteca mpi4py
 from mpi4py import MPI
 
-"""Crear un objeto comunicador
-#===============================
-"""
+# Crear un objeto comunicador
 comunicadores = MPI.COMM_WORLD
 
-
-"""Número Total de procesos
-"""
+# Obtener el número total de procesos
 n_procesos = comunicadores.Get_size()
 
-
-"""Identificador de esté proceso
-"""
-
+# Obtener el identificador de este proceso
 quien_soy = comunicadores.Get_rank()
 
-print("Saludos desde el proceso ", str(quien_soy), "de",str(n_procesos))
+# Imprimir un saludo desde cada proceso
+print("Saludos desde el proceso", str(quien_soy), "de", str(n_procesos))
 
-#Si yo soy el cero hago esto:
-
-if quien_soy ==0:
+# Si este proceso es el proceso 0, hacer esto:
+if quien_soy == 0:
     print("Yo soy el proceso 0")
 
-#Si yo soy el uno hago esto otro
-
-
-elif quien_soy ==1:
+# Si este proceso es el proceso 1, hacer esto otro:
+elif quien_soy == 1:
     print("Yo soy el proceso 1")
 
-
-"""Si yo no soy el uno ni el dos hago esto
-"""
-
+# Si este proceso no es ni el 0 ni el 1, hacer esto:
 else:
-    print("Yo no soy ninguno de los dos primeros proceso")
-
-
+    print("Yo no soy ninguno de los dos primeros procesos")
 
